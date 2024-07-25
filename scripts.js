c89cc2dd6cde79fa4c844a6c6a41ce39e09d2290
Hieu Nguyen <htnguyen1310@gmail.com>
2024-07-25 01:04:07 -0400

    rounding number

--- scripts.js
+++ scripts.js
@@ -24,7 +24,7 @@ function multiply(a, b) {
   return a * b;
 }
 function divide(a, b) {
-  return a / b;
+  return Math.round((a / b) * 1000000000) / 1000000000;
 }
 
 function operate(first, operator, second) {
